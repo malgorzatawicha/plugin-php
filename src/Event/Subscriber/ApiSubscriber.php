ebb8e632bd42845c74c70e77f75d7d4731930d0d
Pawel Maciejewski <pmaciejewski@olx.pl>
2017-04-05 10:42:04 +0200

    api subscriber

--- src/Event/Subscriber/ApiSubscriber.php
+++ src/Event/Subscriber/ApiSubscriber.php
@@ -2,16 +2,76 @@
 namespace Athena\Event\Subscriber;
 
 use Athena\Event\HttpTransactionCompleted;
+use Athena\Event\Proxy\BehatProxy;
+use Athena\Logger\Builder\BddReportBuilder;
+use Athena\Logger\Builder\UnitReportBuilder;
+use Athena\Logger\Interpreter\InterpreterInterface;
+use Athena\Logger\Timer\TimerInterface;
+use Athena\Logger\TrafficLoggerInterface;
+use Athena\Stream\OutputStreamInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
-class ApiSubscriber extends UnitSubscriber
+class ApiSubscriber implements EventSubscriberInterface
 {
+    /**
+     * @var \Athena\Logger\Interpreter\InterpreterInterface
+     */
+    private $interpreter;
+
+    /**
+     * @var BddReportBuilder
+     */
+    private $report;
+
+    /**
+     * @var \Athena\Stream\OutputStreamInterface
+     */
+    private $outputStream;
+
+    /**
+     * @var TrafficLoggerInterface
+     */
+    private $trafficLogger;
+
+    /**
+     * @var \Athena\Logger\Timer\TimerInterface
+     */
+    private $timer;
+
+    /**
+     * @var string
+     */
+    private $currentOutlineTitle;
+
+    /**
+     * @var HttpTransactionCompleted[]
+     */
+    private $afterHttpTransactionEvents = [];
+
+    /**
+     * BddSubscriber constructor.
+     *
+     * @param \Athena\Logger\Interpreter\InterpreterInterface $interpreter
+     * @param \Athena\Stream\OutputStreamInterface            $outputStream
+     * @param \Athena\Logger\Timer\TimerInterface             $timer
+     */
+    public function __construct(
+        InterpreterInterface $interpreter,
+        OutputStreamInterface $outputStream,
+        TimerInterface $timer
+    ) {
+        $this->interpreter = $interpreter;
+        $this->outputStream = $outputStream;
+        $this->report = new UnitReportBuilder();
+        $this->timer = $timer;
+    }
+
     /**
      * @inheritdoc
      */
     public static function getSubscribedEvents()
     {
-        $events = parent::getSubscribedEvents();
-
+        $events = BehatProxy::getSubscribedEvents();
         $events[HttpTransactionCompleted::AFTER] = 'afterComplete';
 
         return $events;
@@ -23,6 +83,17 @@ class ApiSubscriber extends UnitSubscriber
     public function afterComplete(HttpTransactionCompleted $event)
     {
         $this->report->addHttpTransaction($event->getRequest(), $event->getResponse());
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    public function __destruct()
+    {
+        $contents = $this->interpreter->interpret($this->report->build()->toArray());
+
+        $this->outputStream->write($contents);
     }
 }
 
