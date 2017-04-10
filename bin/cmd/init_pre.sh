6e21b33344535c3e196ebc988ab5688e8af1391a
Pawel Maciejewski <pmaciejewski@olx.pl>
2017-04-10 16:04:07 +0200

    bump  twig to 2.3.0 and init_pre check php ver

--- bin/cmd/init_pre.sh
+++ bin/cmd/init_pre.sh
@@ -1,5 +1,18 @@
 CMD_DESCRIPTION="Initialize plugin for the first time."
 
+if ! athena.argument.argument_exists "--php-version"; then
+    athena.plugins.php.set_php_version "5.6"
+else
+    version="$(athena.argument.get_argument --php-version)"
+    athena.argument.remove_argument "--php-version"
+
+    if [[ -z "$version" ]]; then
+        athena.fatal "--php-version must be set to one of the available versions: $(athena.plugins.php.get_supported_php_versions)"
+    else
+        athena.plugins.php.set_php_version "$version"
+    fi
+fi
+
 composer_dir="${ATHENA_COMPOSER_DIR:-"$HOME/.composer"}"
 
 if [[ ! -d "$composer_dir" ]]; then
